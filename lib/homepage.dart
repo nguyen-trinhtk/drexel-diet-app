1b9ce1cd7a7b9b2e797bcb295b1c4f6c9104d6c6
fweamy <baohanmi@gmail.com>
2025-02-13 15:52:52 -0500

    Establish navigation route

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MenuPage extends StatelessWidget {
+  const MenuPage ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +11,12 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Color(0xffffbf65),
           title: const Text('Test Menu Page'),
+          actions: [
+            IconButton(
+              icon: const Icon (Icons.close),
+              onPressed: () {
+              Navigator.pop(context);
+})],
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
