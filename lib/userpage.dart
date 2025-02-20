2d68d781a4775de9dc18d14b69a63dc45140c2fd
Nguyen Trinh <khanhnguyentrinhthi@gmail.com>
2025-02-20 16:17:26 -0500

    Update user page

--- lib/userpage.dart
+++ lib/userpage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+double currentVolume = 3;
 
 class AppColors {
   static const darkblue = Color(0xff22242f);
@@ -7,27 +10,321 @@ class AppColors {
   static const silk = Color(0xfff6f1d1);
 }
 
-class UserPage extends StatelessWidget {
-  const UserPage({Key? key}) : super(key: key);
+class UserPage extends StatefulWidget {
+  const UserPage({super.key});
+
+  @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  int selectedGenderIndex = 0; // Default gender selection
+  final List<String> genders = ["♂️", "♀️"];
+  DateTime? selectedDate;
+  String selectedUnit = "Metric";
+
+  // Separate controllers for different fields
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController heightController = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
+  final TextEditingController goalWeightController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Dispose controllers to free up resources
+    ageController.dispose();
+    heightController.dispose();
+    weightController.dispose();
+    goalWeightController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('User Page'),
-      ),
-      body: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          decoration: BoxDecoration(
-            color: AppColors.yellow,
-            borderRadius: BorderRadius.circular(20)),
-          child: Center(
-            child: Text(
-              'Your mom',
-              style: TextStyle(color: AppColors.darkblue),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            // First row - Welcome Banner
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.lightblue,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.only(left: 80.0, top: 20.0),
+                        child: Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            'Welcome, User',
+                            style: TextStyle(
+                              color: AppColors.silk,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 40.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
+            const SizedBox(height: 20),
+            // Second row - Calories & Preferences
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.yellow,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Calories Left',
+                          style: TextStyle(
+                            color: AppColors.darkblue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.yellow,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Food Preferences',
+                          style: TextStyle(
+                            color: AppColors.darkblue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Third row - Multiple input fields
+            Expanded(
+              flex: 1,
+              child: Row(
+                children: [
+                  _buildTextField('Age', ageController),
+                  _buildTextField('Height', heightController),
+                  _buildTextField('Current Weight', weightController),
+                  _buildTextField('Goal Weight', goalWeightController),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      decoration: BoxDecoration(
+                        color: AppColors.yellow,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Activity Level (1: sedentary, 5: active)",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Slider(
+                            value: currentVolume,
+                            max: 5,
+                            divisions: 5,
+                            label: currentVolume.toString(),
+                            activeColor: AppColors.darkblue,
+                            inactiveColor: AppColors.silk,
+                            onChanged: (double value) {
+                              setState(() {
+                                currentVolume = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  // Gender Selection using Segmented Buttons
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      decoration: BoxDecoration(
+                        color: AppColors.yellow,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Gender",
+                            style: TextStyle(
+                              color: AppColors.darkblue,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          ToggleButtons(
+                            isSelected: List.generate(genders.length,
+                                (index) => index == selectedGenderIndex),
+                            onPressed: (int index) {
+                              setState(() {
+                                selectedGenderIndex = index;
+                              });
+                            },
+                            borderRadius: BorderRadius.circular(10),
+                            selectedColor: Colors.white,
+                            color: AppColors.darkblue,
+                            fillColor: AppColors.darkblue,
+                            children: genders
+                                .map((gender) => Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 12.0),
+                                      child: Text(gender,
+                                          style: const TextStyle(fontSize: 16)),
+                                    ))
+                                .toList(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  // Days to Goal
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      decoration: BoxDecoration(
+                        color: AppColors.yellow,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton.icon(
+                            icon: const Icon(Icons.calendar_today),
+                            onPressed: () async {
+                              var pickedDate = await showDatePicker(
+                                context: context,
+                                initialEntryMode:
+                                    DatePickerEntryMode.calendarOnly,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime(2019),
+                                lastDate: DateTime(2050),
+                              );
+
+                              if (pickedDate != null) {
+                                setState(() {
+                                  selectedDate = pickedDate;
+                                });
+                              }
+                            },
+                            label: const Text('Days until goal'),
+                          ),
+                          Text(
+                            selectedDate == null
+                                ? "Date not picked."
+                                : DateFormat('MM-dd-yyyy')
+                                    .format(selectedDate!),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.yellow,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Unit',
+                              style: TextStyle(
+                                color: AppColors.darkblue,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            DropdownButton<String>(
+                              value: selectedUnit,
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  selectedUnit = newValue!;
+                                });
+                              },
+                              items: [
+                                "Metric",
+                                "Imperial"
+                              ].map<DropdownMenuItem<String>>((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value,
+                                      style:
+                                          TextStyle(color: AppColors.darkblue)),
+                                );
+                              }).toList(),
+                              dropdownColor:
+                                  AppColors.silk, // Dropdown background color
+                              style: TextStyle(
+                                  color: AppColors.darkblue, fontSize: 16),
+                              underline: Container(
+                                  height: 2, color: AppColors.darkblue),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Helper function to create text fields
+  Widget _buildTextField(String label, TextEditingController controller) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.only(right: 10),
+        decoration: BoxDecoration(
+          color: AppColors.yellow,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Center(
+          child: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              labelText: label,
+            ),
+          ),
         ),
       ),
     );
