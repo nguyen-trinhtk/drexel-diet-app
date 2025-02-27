d21dd52067076987dd3dcd590089d83aff3de5d9
Nguyen Trinh <khanhnguyentrinhthi@gmail.com>
2025-02-27 15:30:50 -0500

    Merge main and menu page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -18,77 +18,79 @@ class MenuPage extends StatelessWidget {
           title: const Text('Test Menu Page'),
           actions: [
             IconButton(
-              icon: const Icon (Icons.close),
-              onPressed: () {
-                Navigator.pop(context);
-              })
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color(0xffffbf65),
-              ),
-              child: Text("Menu",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-            ),
-            Builder(
-              // Context
-              builder: (context) => GFButton(
+                icon: const Icon(Icons.close),
                 onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const MenuPage()),
-                  );
-                },
-                text: "Go to Menu",
-                fullWidthButton: true,
-              ),
-            ),
-            Builder(
-              builder: (context) => GFButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const UserPage()));
-                },
-                text: "Go to User Page",
-                fullWidthButton: true,
-              ),
-            ),
+                  Navigator.pop(context);
+                })
           ],
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color(0xffffbf65),
+                ),
+                child: Text("Menu",
+                    style:
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              ),
+              Builder(
+                // Context
+                builder: (context) => GFButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => const MenuPage()),
+                    );
+                  },
+                  text: "Go to Menu",
+                  fullWidthButton: true,
+                ),
+              ),
+              Builder(
+                builder: (context) => GFButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const UserPage()));
+                  },
+                  text: "Go to User Page",
+                  fullWidthButton: true,
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: findCardsPerRow(
+                    viewWidth, 300), // Number of columns in a row
+                crossAxisSpacing: 10, // Space between columns
+                mainAxisSpacing: 10, // Space between rows
+                childAspectRatio: 1.5, // Adjust width/height ratio
+              ),
+              itemCount: 5, // Added item count
+              itemBuilder: (BuildContext context, int index) {
+                return GFCard(
+                  boxFit: BoxFit.cover,
+                  title: GFListTile(
+                    title: Text('Food Name $index'),
+                  ),
+                  content: Text("Food Description"),
+                  buttonBar: GFButtonBar(
+                    children: [
+                      GFButton(
+                        onPressed: () {},
+                        text: 'See more',
+                      ),
+                    ],
+                  ),
+                );
+              },
+            )),
       ),
-      body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount:
-                  findCardsPerRow(viewWidth, 300), // Number of columns in a row
-              crossAxisSpacing: 10, // Space between columns
-              mainAxisSpacing: 10, // Space between rows
-              childAspectRatio: 1.5, // Adjust width/height ratio
-            ),
-            itemCount: 5, // Added item count
-            itemBuilder: (BuildContext context, int index) {
-              return GFCard(
-                boxFit: BoxFit.cover,
-                title: GFListTile(
-                  title: Text('Food Name $index'),
-                ),
-                content: Text("Food Description"),
-                buttonBar: GFButtonBar(
-                  children: [
-                    GFButton(
-                      onPressed: () {},
-                      text: 'See more',
-                    ),
-                  ],
-                ),
-              );
-            },
-          )),
     );
   }
 }
