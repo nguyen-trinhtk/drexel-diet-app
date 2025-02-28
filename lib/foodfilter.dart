d73a5e30c488e7890da6481272e9302626f75cd3
Nguyen Trinh <khanhnguyentrinhthi@gmail.com>
2025-02-28 12:08:14 -0500

    Display food filter in home page

--- lib/foodfilter.dart
+++ lib/foodfilter.dart
@@ -19,8 +19,7 @@ enum FoodPreference {
   vegetarian,
   wholeGrain,
   eatWell,
-  plantForward,
-  none
+  plantForward
 }
 
 class FoodFilterDrawer extends StatefulWidget {
@@ -30,73 +29,79 @@ class FoodFilterDrawer extends StatefulWidget {
   State<FoodFilterDrawer> createState() => _FoodFilterDrawerState();
 }
 
+//Selected choices are in the stationFilters and foodPreferenceFilters sets :)
+
 class _FoodFilterDrawerState extends State<FoodFilterDrawer> {
   Set<Station> stationFilters = <Station>{};
   Set<FoodPreference> foodPreferenceFilters = <FoodPreference>{};
-
+  String formatText(String text) {
+    return text.replaceAllMapped(RegExp(r'([A-Z])'), (Match match) => ' ${match.group(0)}').trim().replaceFirstMapped(RegExp(r'^[a-z]'), (Match match) => match.group(0)!.toUpperCase());
+  }
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.centerRight,
-      child: FractionallySizedBox(
-        widthFactor: 1 / 3,
-        child: Drawer(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Select a station', style: Theme.of(context).textTheme.titleMedium),
-                Wrap(
-                  spacing: 5.0,
-                  children: Station.values.map((Station stationName) {
-                    return FilterChip(
-                      label: Text(stationName.name),
-                      selected: stationFilters.contains(stationName),
-                      onSelected: (bool selected) {
-                        setState(() {
-                          if (selected) {
-                            stationFilters.add(stationName);
-                          } else {
-                            stationFilters.remove(stationName);
-                          }
-                        });
-                      },
-                    );
-                  }).toList(),
-                ),
-                const SizedBox(height: 10.0),
-                Text('Food Preference', style: Theme.of(context).textTheme.titleMedium),
-                Wrap(
-                  spacing: 5.0,
-                  children: FoodPreference.values.map((FoodPreference preference) {
-                    return FilterChip(
-                      label: Text(preference.name),
-                      selected: foodPreferenceFilters.contains(preference),
-                      onSelected: (bool selected) {
-                        setState(() {
-                          if (selected) {
-                            foodPreferenceFilters.add(preference);
-                          } else {
-                            foodPreferenceFilters.remove(preference);
-                          }
-                        });
-                      },
-                    );
-                  }).toList(),
-                ),
-                const SizedBox(height: 20.0),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: const Text('Apply Filters'),
-                  ),
-                ),
-              ],
-            ),
+    return Column(
+      children: <Widget>[
+        const SizedBox(height: 20.0),
+        Text('Station Preference',
+            style: Theme.of(context).textTheme.titleMedium),
+        const SizedBox(height: 20.0),
+        Wrap(
+          spacing: 5.0,
+          runSpacing: 5.0,
+          children: Station.values.map((Station stationName) {
+            return FilterChip(
+              label: Text(formatText(stationName.name)),
+              selected: stationFilters.contains(stationName),
+              onSelected: (bool selected) {
+                setState(() {
+                  if (selected) {
+                    stationFilters.add(stationName);
+                  } else {
+                    stationFilters.remove(stationName);
+                  }
+                });
+              },
+            );
+          }).toList(),
+        ),
+        const SizedBox(height: 20.0),
+        Text('Food Preference', style: Theme.of(context).textTheme.titleMedium),
+        const SizedBox(height: 20.0),
+        Wrap(
+          spacing: 5.0,
+          runSpacing: 5.0,
+          children: FoodPreference.values.map((FoodPreference preference) {
+            return FilterChip(
+              label: Text(formatText(preference.name)),
+              selected: foodPreferenceFilters.contains(preference),
+              onSelected: (bool selected) {
+                setState(() {
+                  if (selected) {
+                    foodPreferenceFilters.add(preference);
+                  } else {
+                    foodPreferenceFilters.remove(preference);
+                  }
+                });
+              },
+            );
+          }).toList(),
+        ),
+        const SizedBox(height: 20.0),
+        Center(
+          child: ElevatedButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Apply Filters'),
           ),
         ),
-      ),
+        const SizedBox(height: 20.0),
+        Text(
+          'Selected Stations: ${stationFilters.map((e) => formatText(e.name)).join(', ')}',
+        ),
+        const SizedBox(height: 20.0),
+        Text(
+          'Selected Food Preferences: ${foodPreferenceFilters.map((e) => formatText(e.name)).join(', ')}',
+        ),
+      ],
     );
   }
 }
