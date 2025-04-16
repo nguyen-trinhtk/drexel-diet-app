f41e22c459bfa58145fed469d320002da175ce12
nt636@tux.cci.drexel.edu <khanhnguyentrinhthi@gmail.com>
2025-04-15 22:03:08 -0400

    Fix sidebar animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:code/pages/home.dart';
 import 'package:code/pages/profile.dart';
-import 'package:code/pages/dietpage.dart';
+import 'package:code/pages/diet.dart';
 import 'package:flutter/material.dart';
 import 'pages/filter.dart';
 
@@ -37,16 +37,16 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 5, vsync: this, initialIndex: 4);
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 0);
 
     _tabController.addListener(() {
-      // Ensures the tab is updated when switching
       if (_tabController.indexIsChanging) {
         setState(() {});
       }
@@ -61,116 +61,96 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        // Sidebar with CustomPaint
-        SizedBox(
-          width: 200,
-          height: double.infinity,
-          child: CustomPaint(
-            painter: ThemedSidebar(width: 200),
-            size: MediaQuery.of(context).size,
-          ),
-        ),
-        Positioned(
-          left: 100,
-          top: 115,
-          child: Container(
-            width: 160,
-            height: 182,
-            color: AppColors.primaryBackground,
-          ),
-        ),
-        Scaffold(
-          backgroundColor: AppColors.transparentBlack,
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              // Sidebar for tabs
-              SizedBox(
-                width: 200,
-                child: RotatedBox(
-                  quarterTurns: 3,
-                  child: TabBar(
-                    labelPadding: EdgeInsets.zero,
-                    controller: _tabController,
-                    overlayColor: WidgetStateProperty.all(Colors.transparent),
-                    dividerColor: Colors.transparent,
-                    indicatorColor: Colors.transparent,
-                    physics: const NeverScrollableScrollPhysics(),
-                    isScrollable: true,
-                    onTap: (index) {
-                      setState(() {
-                        _tabController.animateTo(index);
-                      });
-                    },
-                    tabs: List.generate(5, (index) {
-                      final labels = ['Settings', 'Diet Plans', 'Profile', 'Home'];
-                      if (index == 0) {
-                        return const SizedBox(width: 200, height: 275);
-                      } else {
-                        final label = labels[index - 1];
-                        final isSelected = _tabController.index == index;
-                        final isBeforeSelected = index == _tabController.index - 1;
-                        final isAfterSelected = index == _tabController.index + 1;
-                        return RotatedBox(
-                          quarterTurns: 1,
-                          child: Tab(
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 20),
-                              child: Container(
-                                width: 200,
-                                alignment: Alignment.centerLeft,
-                                padding: const EdgeInsets.only(left: 20),
-                                decoration: BoxDecoration(
-                                  color: isSelected ? AppColors.primaryBackground : AppColors.secondaryBackground,
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(isSelected ? 200 : 0),
-                                    bottomLeft: Radius.circular(isSelected ? 200 : 0),
-                                    topRight: Radius.circular(isBeforeSelected ? 200 : 0),
-                                    bottomRight: Radius.circular(isAfterSelected ? 200 : 0),
-                                  ),
-                                ),
-                                child: CustomText(
-                                  content: label,
-                                  header: true,
-                                  fontSize: 20,
-                                  color: isSelected ? AppColors.accent : AppColors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                        );
-                      }
-                    }),
+    return Scaffold(
+      backgroundColor: AppColors.primaryBackground,
+      body: Row(
+        children: [
+          Container(
+            width: 200,
+            height: double.infinity,
+            decoration: const BoxDecoration(),
+            child: Stack(
+              children: [
+                SizedBox(
+                  width: 200,
+                  height: double.infinity,
+                  child: CustomPaint(
+                    painter: ThemedSidebar(width: 200),
                   ),
                 ),
-              ),
-              Flexible(
-                child: TabBarView(
-                  controller: _tabController, // Ensure TabController is connected here
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: [
-                    Center(child: Text("Blank")),
-                    Center(child: Text("Blank")),
-                    DietPage(),
-                    ProfilePage(),
-                    HomePage(),
-                  ],
+                Container(
+                  width: 100,
+                  height: 200,
+                  margin: const EdgeInsets.only(left: 100),
+                  color: AppColors.primaryBackground
                 ),
-              ),
-            ],
+                Column(
+                    children: List.generate(4, (index) {
+                      final labels = ['Home', 'Profile', 'History', 'Settings'];
+                      final label = labels[index];
+                      final isSelected = _tabController.index == index;
+                      final isBeforeSelected =
+                          index == _tabController.index - 1;
+                      final isAfterSelected = index == _tabController.index + 1;
+                      return Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              _tabController.animateTo(index);
+                            });
+                          },
+                          child: Container(
+                            height: 50,
+                            width: double.infinity,
+                            alignment: Alignment.centerLeft,
+                            padding: const EdgeInsets.only(left: 20),
+                            decoration: BoxDecoration(
+                              color: isSelected
+                                  ? AppColors.primaryBackground
+                                  : AppColors.secondaryBackground,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(isSelected ? 50 : 0),
+                                bottomLeft:
+                                    Radius.circular(isSelected ? 50 : 0),
+                                topRight:
+                                    Radius.circular(isAfterSelected ? 50 : 0),
+                                bottomRight:
+                                    Radius.circular(isBeforeSelected ? 50 : 0),
+                              ),
+                            ),
+                            child: CustomText(
+                              content: label,
+                              header: true,
+                              fontSize: 20,
+                              color: isSelected
+                                  ? AppColors.accent
+                                  : AppColors.white,
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                ),
+              ],
+            ),
           ),
-        ),
-        Positioned(
-          left: 0,
-          bottom: 50,
-          child: AbsorbPointer(
-            child: SizedBox(width: 200, height: 255),
+
+          // Tab content
+          Flexible(
+            child: TabBarView(
+              controller: _tabController,
+              physics: const NeverScrollableScrollPhysics(),
+              children: [
+                HomePage(),
+                ProfilePage(),
+                DietPage(),
+                Center(child: Text("Blank")),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
