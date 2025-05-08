3e135f0242cf918ce176a828b57a0d3ae2d9456d
ssavutu <ssavutu@gmail.com>
2025-05-08 15:37:05 -0400

    Profile Page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../UI/widgets.dart';
 import '../UI/custom_elements.dart';
 import '../UI/colors.dart';
+import '../UI/fonts.dart';
 
 enum Genders { male, female, nonbinary }
 
@@ -22,13 +23,17 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       backgroundColor: AppColors.primaryBackground,
       body: Padding(
-        padding: EdgeInsets.all(32),
+        padding: EdgeInsets.only(top: 48, bottom: 48, right: 64, left: 64),
         child: Row(
+        spacing: 24,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Flexible(
-            flex: 23,
+            flex: 60,
             child: 
           Column(
+            spacing: 24,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Expanded(
               flex: 10,
@@ -176,266 +181,148 @@ class _ProfilePageState extends State<ProfilePage> {
                   ])
               )))
             ],)
+          ),
+          Flexible(
+            flex: 40,
+            child: Column(
+              spacing: 24,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:[
+
+              Expanded(
+              flex:25,
+              child:
+              ThemedCard(
+                padding: EdgeInsets.all(8),
+                child: 
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:
+                      [
+                          Column(
+                            spacing: 2,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              CustomText(
+                                content: "Goal Weight",
+                                softWrap: true,
+                                fontSize: 24,
+                                header: true,
+                              ),
+                              CustomText(
+                                content: "45",
+                                header: true,
+                                color: AppColors.accent,
+                                fontSize: 48
+                              )
+                            ]
+                          )
+                        
+                      ]
+                  ),),),
+              Expanded(
+              flex:25,
+              child:
+              ThemedCard(
+                padding: EdgeInsets.all(8),
+                child: 
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:
+                      [
+                          Column(
+                            spacing: 2,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              CustomText(
+                                content: "Goal Calories",
+                                softWrap: true,
+                                fontSize: 24,
+                                header: true,
+                              ),
+                              CustomText(
+                                content: "1792",
+                                header: true,
+                                color: AppColors.accent,
+                                fontSize: 48
+                              )
+                            ]
+                          )
+                        
+                      ]
+                  ),),),
+              Expanded(
+              flex:25,
+              child:
+              ThemedCard(
+                padding: EdgeInsets.all(8),
+                child: 
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:
+                      [
+                          Column(
+                            spacing: 2,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              CustomText(
+                                content: "Days Until Goal",
+                                softWrap: true,
+                                fontSize: 24,
+                                header: true,
+                              ),
+                              CustomText(
+                                content: "23",
+                                header: true,
+                                color: AppColors.accent,
+                                fontSize: 48
+                              )
+                            ]
+                          )
+                        
+                      ]
+                  ),),),
+              Expanded(
+              flex:25,
+              child:
+              ThemedCard(
+                padding: EdgeInsets.all(8),
+                child: 
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:
+                      [
+                          Column(
+                            spacing: 2,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              CustomText(
+                                content: "Days Achieved",
+                                softWrap: true,
+                                fontSize: 24,
+                                header: true,
+                              ),
+                              CustomText(
+                                content: "5",
+                                header: true,
+                                color: AppColors.accent,
+                                fontSize: 48
+                              )
+                            ]
+                          )
+                        
+                      ]
+                  ),),)
+
+              ]
+            )
           )
-          ,
           ]),
         )
-        
-      /*SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: ThemedCard(
-                  wFactor: 1.3 / 3,
-                  hFactor: 1.3 / 3,
-                  content: [
-                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                      CircleAvatar(
-                        backgroundImage: AssetImage(
-                            "../assets/images/han.jpg"),
-                        radius: 100,
-                      ),
-                      Padding(
-                          padding: EdgeInsets.only(left: 42),
-                          child: Column(children: [
-                            Container(decoration: BoxDecoration(border: Border(bottom: BorderSide(width: 1.5,color: AppColors.primaryText))),
-                              child: CustomText(
-                                content: "Name",
-                                fontSize: 62,
-                                header: true,
-                              )
-                            ),
-                            CustomText(
-                              content: "1-Month Goal Plan",
-                              fontSize: 24
-                            )
-                          ])
-                      )
-                    ])
-                  ]
-                )
-              )
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: ThemedCard(
-                  wFactor: 1.3/3,
-                  hFactor: 1.6/3,
-                  content: [
-                    Padding(padding: EdgeInsets.all(8),
-                    child:Container(
-                      decoration:BoxDecoration(border: Border(bottom: BorderSide(width: 1.5, color: Color(0xFF232597)))), 
-                      child:Row(
-                        mainAxisSize: MainAxisSize.min, 
-                        children:[
-                          CustomText(content: "   User Information ", fontSize: 24, header: true,),
-                          Icon(Icons.edit, size:42, color:Color(0xFF232597)), 
-                          CustomText(content:"  ", fontSize: 26, header: true)
-                        ]
-                    ))),
-                    IntrinsicWidth(stepWidth:550, child:Padding(padding: EdgeInsets.all(8), child:Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children:[
-                        CustomText(content:"Age", fontSize: 24,),
-                        CustomText(content: "18 yrs", fontSize: 24,)
-                      ]
-                    ))),
-                    IntrinsicWidth(stepWidth: 550, child:Padding(padding: EdgeInsets.all(8), child:Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children:[
-                        CustomText(content:"Height", fontSize: 24,),
-                        CustomText(content: "182 cm", fontSize: 24,)
-                      ]
-                    ))),
-                    IntrinsicWidth(stepWidth:550, child:Padding(padding:EdgeInsets.all(8) , child:Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children:[
-                        CustomText(content:"Current Weight", fontSize: 24,),
-                        CustomText(content:"50 kg", fontSize: 24,),
-                      ]
-                    ))),
-                    IntrinsicWidth(stepWidth:550, child:Padding(padding:EdgeInsets.all(8), child:Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children:[
-                        CustomText(content:"Activity Level", fontSize: 24,),
-                        SliderTheme(
-                          data: SliderTheme.of(context).copyWith(
-                            activeTickMarkColor: Color(0x00FFFFFF),
-                            inactiveTickMarkColor: Color(0x00FFFFFF),
-                            thumbColor: Color(0xFFE66D8D),
-                            valueIndicatorColor: Color(0xFFE66D8D),
-                            activeTrackColor: Color(0xFF232597),
-                            inactiveTrackColor: Color(0xFF232597),
-                            trackHeight: 2,
-                            trackShape: RectangularSliderTrackShape()
-                          ), 
-                          child:Slider(
-                            value: selectedIndex.toDouble(),
-                            min: 0,
-                            max: 5,
-                            divisions: 5,
-                            label: selectedIndex.toString(),
-                            onChanged: (double value) {
-                              setState(() {
-                                    selectedIndex = value.toInt();
-                              });
-                            },
-                          ),
-                        )
-                      ]
-                    ))),
-                    IntrinsicWidth(stepWidth: 550, child:Padding(padding:EdgeInsets.all(8), child:Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children:[
-                        CustomText(content:"Gender", fontSize: 24, ),
-                        SegmentedButton<Genders>(
-                          showSelectedIcon: false,
-                          style: SegmentedButton.styleFrom(
-                            selectedBackgroundColor: Color(0xFFE66D8D),
-                            selectedForegroundColor: Color(0xFFFFFFFF),
-                            foregroundColor: Color(0xFF232597),
-                            backgroundColor: Color(0xFFFFFFFF),
-                            textStyle: TextStyle(fontFamily: AppFonts.headerFont),
-                            side: const BorderSide(
-                              color: Color(0xFF232597)
-                            )
-                          ),
-                          segments: const <ButtonSegment<Genders>>[
-                              ButtonSegment<Genders>(value: Genders.female, label: Text('F')),
-                              ButtonSegment<Genders>(value: Genders.male, label: Text('M')),
-                              ButtonSegment<Genders>(value: Genders.nonbinary, label: Text('N/A')),
-                          ],
-                          selected: selection,
-                          onSelectionChanged: (Set<Genders> newSelection) {
-                            setState(() {
-                              selection = newSelection;
-                            });
-                          },
-                        )
-                      ]
-                    )))
-                  ],
-                )
-              )
-            )
-          ]
-        ),
-      )*/
     );
   }
 }
-/*      
-        Container(
-          padding: EdgeInsets.all(32),
-          child:Card(
-            color: Colors.white,
-            shape: RoundedRectangleBorder(side: BorderSide(width: 1, color: AppColors.primaryText), borderRadius: BorderRadius.circular(40)),
-            semanticContainer: true,
-            child: FractionallySizedBox(
-              widthFactor: 1.6/3,
-              heightFactor: 1.6/3,
-              child:Column( 
-              children: [
-                  Padding(padding: EdgeInsets.all(8),
-                   child:Container(
-                    decoration:BoxDecoration(border: Border(bottom: BorderSide(width: 1.5, color: Color(0xFF232597)))), 
-                    child:Row(
-                      mainAxisSize: MainAxisSize.min, 
-                      children:[
-                        CustomText(content: "   User Information ", fontSize: 24, header: true,),
-                        Icon(Icons.edit, size:42, color:Color(0xFF232597)), 
-                        CustomText(content:"  ", fontSize: 26, header: true)
-                      ]
-                  ))),
-                  IntrinsicWidth(stepWidth:550, child:Padding(padding: EdgeInsets.all(8), child:Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children:[
-                      CustomText(content:"Age", fontSize: 24,),
-                      CustomText(content: "18 yrs", fontSize: 24,)
-                    ]
-                  ))),
-                  IntrinsicWidth(stepWidth: 550, child:Padding(padding: EdgeInsets.all(8), child:Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children:[
-                      CustomText(content:"Height", fontSize: 24,),
-                      CustomText(content: "182 cm", fontSize: 24,)
-                    ]
-                  ))),
-                  IntrinsicWidth(stepWidth:550, child:Padding(padding:EdgeInsets.all(8) , child:Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children:[
-                      CustomText(content:"Current Weight", fontSize: 24,),
-                      CustomText(content:"50 kg", fontSize: 24,),
-                    ]
-                  ))),
-                  IntrinsicWidth(stepWidth:550, child:Padding(padding:EdgeInsets.all(8), child:Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children:[
-                      CustomText(content:"Activity Level", fontSize: 24,),
-                      SliderTheme(
-                        data: SliderTheme.of(context).copyWith(
-                          activeTickMarkColor: Color(0x00FFFFFF),
-                          inactiveTickMarkColor: Color(0x00FFFFFF),
-                          thumbColor: Color(0xFFE66D8D),
-                          valueIndicatorColor: Color(0xFFE66D8D),
-                          activeTrackColor: Color(0xFF232597),
-                          inactiveTrackColor: Color(0xFF232597),
-                          trackHeight: 2,
-                          trackShape: RectangularSliderTrackShape()
-                        ), 
-                        child:Slider(
-                          value: selectedIndex.toDouble(),
-                          min: 0,
-                          max: 5,
-                          divisions: 5,
-                          label: selectedIndex.toString(),
-                          onChanged: (double value) {
-                            setState(() {
-                                  selectedIndex = value.toInt();
-                            });
-                          },
-                        ),
-                      )
-                    ]
-                  ))),
-                  IntrinsicWidth(stepWidth: 550, child:Padding(padding:EdgeInsets.all(8), child:Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children:[
-                      CustomText(content:"Gender", fontSize: 24, ),
-                      SegmentedButton<Genders>(
-                        showSelectedIcon: false,
-                        style: SegmentedButton.styleFrom(
-                          selectedBackgroundColor: Color(0xFFE66D8D),
-                          selectedForegroundColor: Color(0xFFFFFFFF),
-                          foregroundColor: Color(0xFF232597),
-                          backgroundColor: Color(0xFFFFFFFF),
-                          textStyle: TextStyle(fontFamily: AppFonts.headerFont),
-                          side: const BorderSide(
-                            color: Color(0xFF232597)
-                          )
-                        ),
-                        segments: const <ButtonSegment<Genders>>[
-                            ButtonSegment<Genders>(value: Genders.female, label: Text('F')),
-                            ButtonSegment<Genders>(value: Genders.male, label: Text('M')),
-                            ButtonSegment<Genders>(value: Genders.nonbinary, label: Text('N/A')),
-                        ],
-                        selected: selection,
-                        onSelectionChanged: (Set<Genders> newSelection) {
-                          setState(() {
-                            selection = newSelection;
-                          });
-                        },
-                      )
-                    ]
-                  )))
-                ]
-              )
-            )
-          )
-        )])*/
