5efd1ac95d32c05cbed1ee577a7b113abc17bd11
fweamy <baohanmi@gmail.com>
2025-06-03 16:56:04 -0400

    Fix Gemini prompt

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -154,11 +154,12 @@ class _HomepageState extends State<HomePage> {
     final menuData = globalData.menuData;
 
     List<String> recommendedIndex = [];
+    var userDishes = "Honey BBQ Chicken";
 
     Gemini.init(apiKey: apiKey ?? "");
     await Gemini.instance.prompt(parts: [
       Part.text(
-          'Based on user past data, suggest 1-3 dishes from the current menu data. ONLY output your response with only the ID number of the dish (e.g. 1, 2, 3). Here is user past data: Honey BBQ Chicken. Here is menu data: $menuData'),
+          'Based on user past data, suggest 1-3 dishes from the current menu data, don\'t suggest overlapping dishes twice (e.g. Only suggest Grilled Chicken Breast once). ONLY output your response with only the ID number of the dish (e.g. 1, 2, 3). Here is user past data: $userDishes. Here is menu data: $menuData'),
     ]).then((value) {
       var ans = value?.output;
       print("Answer from Gemini: $ans");
